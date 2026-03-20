7cc1825fbd0d0e41a02d49e7d87a55147ec1dc85
VincebusRiveruptum <q3map_2@live.cl>
2026-03-20 20:28:52 -0300

    chg: custom local font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,11 +6,14 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         <!-- Scripts -->
+        <script>
+            if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark')
+            }
+        </script>
         @routes
         @vite(['resources/js/app.js', "resources/js/modules/{$page['component']}.vue"])
         @inertiaHead
